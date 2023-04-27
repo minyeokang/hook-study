6b454540053ca8893479ed486e5c021ce2d1174b
minyeo0729 <minyeo@studio-jt.co.kr>
2023-04-27 10:44:52 +0900

    useRef - functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const inputRef = useRef()
+  useEffect(()=>{
+    inputRef.current.focus()
+    console.log(inputRef.current.value)
+  })
+  const login = () => {
+    alert(`hello ${inputRef.current.value}`)
+    inputRef.current.value = ''
+    inputRef.current.focus()
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <input ref={inputRef} type="text"placeholder='name' />
+       <button onClick={login}></button>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='hello'>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='count-btn' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
