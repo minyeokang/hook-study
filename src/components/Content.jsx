4e8c1a66f0b12905d190b33e512e28ec53cd2cfb
minyeo0729 <minyeo@studio-jt.co.kr>
2023-04-28 14:53:14 +0900

    useMemo

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { useMemo } from 'react'
+import { useEffect } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 import { UserContext } from '../context/UserContext'
 
 const Content = () => {
     const {isDark} = useContext(ThemeContext)
     const user = useContext(UserContext)
+
+    const [number, setNumber] = useState(0);
+    const [isKorea, setIsKorea] = useState(true)
+    const location = useMemo(()=>{
+        return { 
+            country : isKorea ? 'korea' : 'thailand'
+        }
+    }, [isKorea])
+   
+
+        
+    useEffect(()=>{
+        console.log('test')
+    },[location])
   return (
+    <>
+    <p>increase number</p>
+    <input type="number" value={number} onChange={(e)=>setNumber(e.target.value)} />
+    <p>country: {location.country}</p>
+    <button onClick={()=>setIsKorea(!isKorea)}>change</button>
     <div className="content" style={{
         backgroundColor: isDark ? 'black' : 'white',
         color: isDark ? 'white' : 'black'
     }}>
         <p>have a nice day {user}</p>
     </div>
+        </>
   )
 }
 
